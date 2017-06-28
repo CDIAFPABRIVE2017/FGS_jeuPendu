5bfade487bcb91add9050d7e5f0bc398b23f5040
sarahcsn <chesnesarah@gmail.com>
2017-06-28 14:49:46 +0200

    Modifs Manche
    méthodes et propriétés

--- JeuPendu_windowsforms/FrmJeu.Designer.cs
+++ JeuPendu_windowsforms/FrmJeu.Designer.cs
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Jeu
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // FrmJeu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Jeu";
+            this.ClientSize = new System.Drawing.Size(607, 508);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmJeu";
             this.Text = "Jeu du Pendu";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 

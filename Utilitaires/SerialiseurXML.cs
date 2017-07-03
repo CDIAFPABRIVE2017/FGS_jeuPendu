b4e5f09c7e59c7fcbeff25aded44bc7b7223a97b
ifazia <iguetf@gmail.com>
2017-07-03 10:14:33 +0200

    modif020717

--- Utilitaires/SerialiseurXML.cs
+++ Utilitaires/SerialiseurXML.cs
@@ -14,11 +14,11 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Utilitaires
 {
-   
+
     public class SauvegardeXML : ISauvegarde
     {
 
-    
+
         /// <summary>
         /// Sauvegarder par sérialisation Xml 
         /// </summary>
@@ -26,16 +26,40 @@ namespace Utilitaires
         /// <param name="objetASauvegarder"></param>
         public void Save(string pathRepData, IEnumerable objetASauvegarder)
         {
-            Type type = objetASauvegarder.GetType();
 
-            string pathXmlDocument = string.Format("{0}\\{1}.Xml", pathRepData, type.FullName);
-            using (FileStream fileStream = new FileStream(pathXmlDocument, FileMode.Create, FileAccess.Write, FileShare.Read))
+            // Enregistrer le fichier text
+            string extention = string.Empty;
+            if (pathRepData.Length >= 4)
             {
-                XmlTextWriter xmlTW = new XmlTextWriter(fileStream, Encoding.UTF8);
-                XmlSerializer xmlS = new XmlSerializer(type);
-                xmlS.Serialize(xmlTW, objetASauvegarder);
-                xmlTW.Close();
-                fileStream.Close();
+                extention = pathRepData.Substring(pathRepData.Length - 4, 4).ToString().Trim();
+            }
+
+            if (string.Equals(extention, ".txt"))
+            {
+                using (StreamWriter sw = new StreamWriter(pathRepData))
+                {
+                    foreach (string item in objetASauvegarder)
+                        sw.WriteLine(item);
+
+                    sw.Close();
+                }
+
+
+            }
+            else
+            {
+                // Enregistrer le fichier xml
+                Type type = objetASauvegarder.GetType();
+
+                string pathXmlDocument = string.Format("{0}\\{1}.Xml", pathRepData, type.FullName);
+                using (FileStream fileStream = new FileStream(pathXmlDocument, FileMode.Create, FileAccess.Write, FileShare.Read))
+                {
+                    XmlTextWriter xmlTW = new XmlTextWriter(fileStream, Encoding.UTF8);
+                    XmlSerializer xmlS = new XmlSerializer(type);
+                    xmlS.Serialize(xmlTW, objetASauvegarder);
+                    xmlTW.Close();
+                    fileStream.Close();
+                }
             }
         }
         /// <summary>
@@ -47,30 +71,50 @@ namespace Utilitaires
         public IEnumerable Load(string pathRepData, Type typeACharger)
         {
             Object objet = new object();
-            string pathXmlDocument = string.Format("{0}\\{1}.Xml", pathRepData, typeACharger.FullName);
-
-            if (!File.Exists(pathXmlDocument))
+            // lire fichier text
+            string extention = pathRepData.Substring(pathRepData.Length - 4, 4).ToString().Trim();
+            if (string.Equals(extention, ".txt"))
             {
-                File.Create(pathXmlDocument);
-       
-                
-            }
-
-           
-            using (FileStream fileStream = new FileStream(pathXmlDocument, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                if ((fileStream.Length > 0))
+                List<string> lst = new List<string>();
+                using (var fileStream = File.OpenRead(pathRepData))
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
                 {
-                    XmlTextReader xmlTR = new XmlTextReader(fileStream);
-                    XmlSerializer xmlS = new XmlSerializer(typeACharger);
-
-                    objet = xmlS.Deserialize(xmlTR);
-                    xmlTR.Close();
+                    String line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        lst.Add(line);
+                    }
+                    fileStream.Close();
+                    streamReader.Close();
+                    return lst as IEnumerable;
                 }
-                
-                fileStream.Close();
             }
-       
+            else
+            {
+                //lecture fichier xml
+                string pathXmlDocument = string.Format("{0}\\{1}.Xml", pathRepData, typeACharger.FullName);
+                if (!File.Exists(pathXmlDocument))
+                {
+                    File.Create(pathXmlDocument);
+
+                }
+
+
+                using (FileStream fileStream = new FileStream(pathXmlDocument, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    if ((fileStream.Length > 0))
+                    {
+                        XmlTextReader xmlTR = new XmlTextReader(fileStream);
+                        XmlSerializer xmlS = new XmlSerializer(typeACharger);
+
+                        objet = xmlS.Deserialize(xmlTR);
+                        xmlTR.Close();
+                    }
+
+                    fileStream.Close();
+                }
+            }
+
             return objet as IEnumerable;
         }
     }
